7791876e24e64310fa44e3f97fc09139cf5f7919
Willian Nogueira <125921653+williands123@users.noreply.github.com>
2023-07-22 17:06:40 -0300

    1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -145,7 +145,7 @@ def clean_code(df1):
 #==============================================================================================================
 
 #importe dataset
-df1 = pd.read_csv('dataset\train.csv' )
+df1 = pd.read_csv('..\dataset\train.csv' )
 
 #limpando dados
 df1=clean_code(df1)
